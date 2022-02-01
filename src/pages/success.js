8cffb94c4708f9441d3dc40386821d3fb611d51a
Tracy Tucker <tracytuckeremail@gmail.com>
2022-01-31 18:30:56 -0600

    Creation of Success submission page

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,10 +1,19 @@
+import { Link } from 'gatsby';
 import React from 'react';
+import Layout from '../components/Layout';
 
 const Success = () => {
   return (
-      <div>
-          <h1>Success page</h1>
-      </div>
+      <Layout>
+        <section className='success-page'>
+          <div className='page-center'>
+            <h2>Submission success!</h2>
+            <Link to='/' className='btn'>
+              Back Home
+            </Link>
+          </div>
+        </section>
+      </Layout>
   )
 };
 
