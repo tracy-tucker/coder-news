7c1f2b4248a30bbc278176c9d0502be81c9e370c
Tracy Tucker <tracytuckeremail@gmail.com>
2022-01-31 18:28:05 -0600

    Creating content for ErrorPage

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,20 @@
+import { Link } from 'gatsby';
 import React from 'react';
+import Layout from '../components/Layout';
 
 const ErrorPage = () => {
   return (
-    <div>
-        <h1>Error Page</h1>
-    </div>
+    <Layout>
+      <section className='error-page'>
+        <div className="page-center">
+          <span>404</span>
+          <h3>This page cannot be found</h3>
+          <Link to='/' className='btn'>
+            Back Home
+          </Link>
+        </div>
+      </section>
+    </Layout>
   )
 };
 
