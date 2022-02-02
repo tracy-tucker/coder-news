286623cc9869909aef41817f5f8b6f3c5364bbeb
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-02 17:30:23 -0600

    Configuring a filesystem for locally stored assets and posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,20 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-remark-images`,
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-mdx`,
       options: {
         gatsbyRemarkPlugins: [
