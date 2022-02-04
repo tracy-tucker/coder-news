8f3c4b6d329e9d7a1f5b66c9a23b94d23f249769
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-04 08:17:55 -0600

    Creating About comp with basic author info and social links

---
+++ src/components/Banner/About.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import SocialLinks from '../../constants/socialLinks';
+import { StaticImage } from 'gatsby-plugin-image';
+// import Title from './Title';
+import styled from 'styled-components';
+
+const About = () => {
+  return (
+      <Wrapper>
+          <StaticImage
+            src='../../assets/banner-about.jpeg'
+            layout='fixed'
+            width={100}
+            height={100}
+            alt='author'
+            className='img'
+          />
+          <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          </p>
+          <SocialLinks styleClass='banner-icons'></SocialLinks>
+      </Wrapper>
+  )
+};
+
+const Wrapper = styled.div`
+    text-align: center;
+    p {
+        color: var(--clr-grey-5);
+    }
+    .img {
+        border-radius: 50%;
+        margin: 0 auto;
+        margin-bottom: 1rem;
+    }
+  `
+
+export default About;
