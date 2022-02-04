e0327a908a0a0612abad3289864e679b9564f780
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-04 11:36:03 -0600

    Creating skeleton template components for post-templage and category-template

---
+++ src/templates/category-template.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Hero from '../components/Hero';
+import Posts from '../components/Posts';
+import { graphql } from 'gatsby';
+
+const CategoryTemplate = () => {
+  return <div>This is the Category Template</div>;
+};
+
+export default CategoryTemplate;
---
+++ src/templates/post-template.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostTemplate = () => {
+  return <div>This is the post template</div>;
+};
+
+export default PostTemplate;
