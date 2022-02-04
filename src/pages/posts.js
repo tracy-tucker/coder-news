07cf2e683168f04c9e7bee48918ac2ecfbe22cc2
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-04 08:00:55 -0600

    Querying posts, then passing posts over to the Posts Component

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import Layout from '../components/Layout';
+import Hero from '../components/Hero';
+import { graphql } from 'gatsby';
+import Posts from '../components/Posts';
 
-const Posts = () => {
+const PostsPage = ({data}) => {
+
+  const {
+    allMdx:{nodes:posts}
+  } = data
+
   return (
-    <div>
-      <h1>Posts page</h1>
-    </div>
+    <Layout>
+      <Hero />
+      <Posts posts={posts} />
+    </Layout>
   )
 };
 
-export default Posts;
+export const query = graphql`
+{
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    nodes {
+      id
+      frontmatter {
+        title
+        author
+        category
+        readTime
+        slug
+        date(formatString: "MMMM, Do, YYYY")
+        image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+      excerpt
+    }
+  }
+}
+`
+
+export default PostsPage;
