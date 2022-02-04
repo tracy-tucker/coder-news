1d1866909ce4f7ff11d50b21688f47c1bee228fe
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-04 09:58:03 -0600

    Querying data to pull unique categories, destructuring rsult to render within Categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import { Link } from 'gatsby';
+
+const query = graphql`
+  {
+    allMdx {
+      distinct(field: frontmatter___category)
+    }
+  }
+`
 
 const Categories = () => {
+
+  const {
+    allMdx:{distinct}
+  } = useStaticQuery(query)
+
   return (
-      <h3>These are categories</h3>
+      <ul className='categories'>
+        {distinct.map((category, index) => {
+          return <li key={index}>
+            <Link to={`/${category}`} className='category'>
+              {category}
+            </Link>
+          </li>
+        })}
+      </ul>
   )
 };
 
