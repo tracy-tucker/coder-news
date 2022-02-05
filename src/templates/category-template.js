4818a2ae418a0586efcf176ff9eca46adc614a1e
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-05 12:02:28 -0600

    Creating category template by querying matching post category

--- src/templates/category-template.js
+++ src/templates/category-template.js
@@ -4,8 +4,50 @@ import Hero from '../components/Hero';
 import Posts from '../components/Posts';
 import { graphql } from 'gatsby';
 
-const CategoryTemplate = () => {
-  return <div>This is the Category Template</div>;
+const CategoryTemplate = (props) => {
+
+  const {
+    data:{allMdx:{nodes:posts}}
+  } = props
+
+  const {
+    pageContext:{category}
+  } = props
+
+  console.log(props)
+
+  return (
+    <Layout>
+      <Hero />
+      <Posts posts={posts} title={`category / ${category}`} />
+    </Layout>
+  )
 };
+
+export const query = graphql`
+  query GetCategoryPosts($category:String) {
+    allMdx(
+      sort: {fields: frontmatter___date, order: DESC}
+      filter: {frontmatter: {category: {eq: $category}}}
+    ) {
+      nodes {
+        id
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          slug
+          date(formatString: "MMMM Do, YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default CategoryTemplate;
