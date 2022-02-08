be626f19f1e3801b3a9df4ce4d3a14d8950a3b81
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-07 18:11:00 -0600

    Creating Headings comp to be added to blog posts

---
+++ src/components/Inserts/Headings.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Headings = ({ children, title }) => {
+    //
+};
+
+const HeadingTwo = style.div`
+margin: 2rem 0;
+  .underline {
+    width: 5rem;
+    height: 5px;
+    background: var(--clr-primary-5);
+  }
+`
+
+export default Headings;
