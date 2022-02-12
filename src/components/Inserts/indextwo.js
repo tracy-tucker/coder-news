3a7357a0acecc571fbdb311959843c2da2be7d74
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-12 06:56:22 -0600

    Adding Inserts index

---
+++ src/components/Inserts/indextwo.js
@@ -0,0 +1,18 @@
+import Counter from './Counter';
+import LikeButton from './LikeButton';
+import { RegVideo, Video } from './Video';
+import * as Headings from './Headings';
+import Code from './Code';
+import Blockquote from './Blockquote';
+import PrismWrapper from './PrismWrapper';
+
+export {
+    Counter,
+    LikeButton,
+    RegVideo,
+    Video,
+    Headings,
+    Code,
+    Blockquote,
+    PrismWrapper,
+}
