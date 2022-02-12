fa2328d990a518d5425c463bee8624b3aa2db32b
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-11 18:41:43 -0600

    Removing uncommented code

--- root-mdx.js
+++ root-mdx.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-import { Headings, Code, Blockquote, ExampleCode, PrismWrapper } from './src/components/Inserts'
+import { Headings, Code, Blockquote, PrismWrapper } from './src/components/Inserts'
 
 const components = {
     h2: Headings.myH2,
--- src/components/Inserts/Index.js
+++ src/components/Inserts/Index.js
@@ -15,7 +15,6 @@ export {
     Headings,
     Code,
     Blockquote,
-    // ExampleCode,
     PrismWrapper,
 }
 
